f84da1eb81db8d4221146cb2e868f21325db39ef
Erick Martins <erickrmartins999@gmail.com>
2026-07-07 22:15:47 -0300

    Spring Initializr

---
+++ apiRestLivraria/src/main/java/com/martins/apiRestLivraria/ApiRestLivrariaApplication.java
@@ -0,0 +1,13 @@
+package com.martins.apiRestLivraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestLivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestLivrariaApplication.class, args);
+	}
+
+}
---
+++ apiRestLivraria/src/test/java/com/martins/apiRestLivraria/ApiRestLivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martins.apiRestLivraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestLivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
